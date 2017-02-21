4248d00d4c0041232cd9d039d0a08405193e07af
Nanchen Yang <ncyang@thoughtworks.com>
2017-02-21 23:07:48 +0800

    sample code.

--- HelloReactNative/index.ios.js
+++ HelloReactNative/index.ios.js
@@ -24,6 +24,73 @@ class Bananas extends Component {
   }
 }
 
+class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}</Text>
+        );    
+    }    
+}
+
+class LotsOfGreetings extends Component {
+    render()  {
+        return (
+            <View stype={{alignItems: 'center'}}>
+                <Greeting name='Rexxar' />
+                <Greeting name='Jaina' />
+                <Greeting name='Valeera' />
+            </View>
+        );    
+    }   
+}
+
+class Blink extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { showText: true };
+
+        setInterval(() => {
+            this.setState({ showText: !this.state.showText });    
+        }, 1000);
+    }    
+
+    render() {
+        let display = this.state.showText ? this.props.text : ' ';
+        return (
+            <Text>{display}</Text>
+        );
+    }
+}
+
+class BlinkApp extends Component {
+    render() {
+        return (
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
+            </View>
+        );
+    }   
+}
+
+
+class LotsOfStyles extends Component {
+
+render() {
+        return (
+            <View>
+                <Text stype={styles.red}>just red</Text>
+                <Text style={styles.bigblue}>just bigblue</Text>
+                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+                <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+            </View>
+        );    
+    }    
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -40,6 +107,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',    
   },
 });
 
@@ -62,4 +137,4 @@ export default class HelloReactNative extends Component {
   }
 }
 
-AppRegistry.registerComponent('HelloReactNative', () => Bananas);
+AppRegistry.registerComponent('HelloReactNative', () => LotsOfStyles);
