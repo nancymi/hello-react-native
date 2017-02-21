2808a7561990b7d4a340322228a1c73dbc70edbd
Nanchen Yang <ncyang@thoughtworks.com>
2017-02-21 23:19:50 +0800

    use [flex] to define flexable size.

--- HelloReactNative/index.ios.js
+++ HelloReactNative/index.ios.js
@@ -102,6 +102,18 @@ class FixedDimensionsBasics extends Component {
     }   
 }
 
+class FlexDimensionsBasics extends Component {
+    render()  {
+        return (
+            <View style={{flex: 1}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+            </View>
+        );    
+    }   
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -148,4 +160,4 @@ export default class HelloReactNative extends Component {
   }
 }
 
-AppRegistry.registerComponent('HelloReactNative', () => FixedDimensionsBasics);
+AppRegistry.registerComponent('HelloReactNative', () => FlexDimensionsBasics);
